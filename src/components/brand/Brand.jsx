af0404eda736f23b650b000651ef3c4ceaeebe0c
Borshon <saha11borshon@gmail.com>
2023-04-18 02:06:35 +0600

    header and brand section done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import './brand.css';
+import React from "react";
+import "./brand.css";
+import { google, atlassian, dropbox, shopify, slack } from "./import";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt3__brand">
+      <div>
+        <img className="google" src={google} alt="google" />
+      </div>
+      <div>
+        <img className="atlassian" src={atlassian} alt="atlassian" />
+      </div>
+      <div>
+        <img className="slack" src={slack} alt="slack" />
+      </div>
+      <div>
+        <img className="dropbox" src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img className="shopify" src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import "./feature.css";
 
-const Feature = () => {
-  return <div>Feature</div>;
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__features_container-feature">
+      <div className="gpt3__features_container-feature_title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features_container-feature_text">
+        <p>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
